fc9ec77c9cdc24ab96508ed2af6f731f8d95fecb
Michael Fehrer <112262472+mtfehrer@users.noreply.github.com>
2025-09-25 18:09:06 -0400

    add robot as cube

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ import time
 import pybullet_data
 
 physicsClient = p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0, 0, -10)
+
 planeId = p.loadURDF("plane.urdf")
-startPos = [0, 0, 1]
-startOrientation = p.getQuaternionFromEuler([0, 0, 0])
-boxId = p.loadURDF("r2d2.urdf", startPos, startOrientation)
+visualShapeId = p.createVisualShape(shapeType=p.GEOM_BOX, halfExtents=[0.5, 0.5, 0.5], rgbaColor=[1, 0, 0, 1])
+collisionShapeId = p.createCollisionShape(shapeType=p.GEOM_BOX, halfExtents=[0.5, 0.5, 0.5])
+
+p.createMultiBody(
+    baseMass=1,
+    baseVisualShapeIndex=visualShapeId,
+    baseCollisionShapeIndex=collisionShapeId,
+    basePosition=[0, 0, 1],
+)
 
 for i in range(10000):
     p.stepSimulation()
