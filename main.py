4742c6db3b7c8d135666c28d21aa586a80ee26da
Michael Fehrer <mtfehrer@gmail.com>
2025-09-26 12:07:46 -0400

    update robot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pybullet as p
 import time
+import math
+import pybullet as p
 import pybullet_data
 
 physicsClient = p.connect(p.GUI)
@@ -8,17 +9,54 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0, 0, -10)
 
 planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("r2d2.urdf", [0, 0, 0.5], p.getQuaternionFromEuler([0,0,0]))
+
+goal = (5, 6)
+force = 100
+velocity = 50
+
+#place goal marker
 visualShapeId = p.createVisualShape(shapeType=p.GEOM_BOX, halfExtents=[0.5, 0.5, 0.5], rgbaColor=[1, 0, 0, 1])
 collisionShapeId = p.createCollisionShape(shapeType=p.GEOM_BOX, halfExtents=[0.5, 0.5, 0.5])
-
 p.createMultiBody(
     baseMass=1,
     baseVisualShapeIndex=visualShapeId,
     baseCollisionShapeIndex=collisionShapeId,
-    basePosition=[0, 0, 1],
+    basePosition=[goal[0], goal[1], 2],
 )
 
-for i in range(10000):
+while True:
+    pos, orn = p.getBasePositionAndOrientation(robotId)
+    x, y, z = pos
+    yaw = p.getEulerFromQuaternion(orn)[2]
+
+    dx = goal[0] - x
+    dy = goal[1] - y
+    distance = math.sqrt(dx*dx + dy*dy)
+    target_angle = math.atan2(dy, dx)
+    angle_error = target_angle - yaw
+    angle_error = math.atan2(math.sin(angle_error), math.cos(angle_error))
+
+    if distance < 1:  # close enough
+        left_vel = right_vel = 0
+    elif abs(angle_error) > 0.2:  # turn towards goal
+        left_vel = -velocity if angle_error > 0 else velocity
+        right_vel = velocity if angle_error > 0 else -velocity
+    else:  # go straight
+        left_vel = right_vel = velocity
+
+    #debug
+    line_length = 2
+    start = [x, y, z]
+    end = [x + line_length * math.cos(yaw),
+           y + line_length * math.sin(yaw),
+           z]
+    p.addUserDebugLine(start, end, [0, 1, 0], 2, lifeTime=0.1)
+
+    # 2 right_front_wheel_joint
+    # 6 left_front_wheel_joint
+    p.setJointMotorControl2(robotId, 2, p.VELOCITY_CONTROL, targetVelocity=right_vel, force=force)
+    p.setJointMotorControl2(robotId, 6, p.VELOCITY_CONTROL, targetVelocity=left_vel, force=force)
     p.stepSimulation()
     time.sleep(1 / 240)
 
